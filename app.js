a3c5ad6c830aa97a39a878336ed1e282a1585e11
Cody Reynolds <codyreynolds@hey.com>
2021-06-25 16:16:45 -0400

    Update commentary in app.js

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const app = express();
 
+//Imports the route files
 const routes = require('./routes/index');
 const books = require('./routes/books')
 
-const app = express();
-
+//Imports the instance of sequelize instantiated in index.js within the models folder
 const {sequelize} = require('./models/index');
 
-// view engine setup
+//View engine setup
+//Tells the app the 'views' folder is the starting point for all the view templates3
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -19,13 +21,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Tells the app the 'public' folder is the starting point for all the static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Delegates routing at the root route to 'routes/index.js' imported above
 app.use('/', routes);
+//Delegates routing from /books path to 'routes/books.js' imported above
 app.use('/books', books);
 
-//catch 404 and forward to error handler
+//Catches 404 errors
 app.use(function(req, res, next) {
   const err = new Error;
     err.status = 404;
@@ -33,7 +38,7 @@ app.use(function(req, res, next) {
   res.status(404).render('page-not-found', {err});
 });
 
-// error handler
+//Global error handler
 app.use(function(err, req, res, next) {
   if(err.status === 404){
     console.log('404 handler called')
@@ -44,7 +49,7 @@ app.use(function(err, req, res, next) {
   }
 });
 
-//Tests the connection to the database.
+//Tests the connection to the database
 (async () => {
   try {
     await sequelize.authenticate();
