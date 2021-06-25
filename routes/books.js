a3cde7235848dc3c71deeb9b0799a05701c460bf
Cody Reynolds <codyreynolds@hey.com>
2021-06-25 16:32:28 -0400

    Enable new book GET route

--- routes/books.js
+++ routes/books.js
@@ -5,21 +5,27 @@ const router = express.Router();
 const Book = require('../models').Book;
 
 /* Handler function to wrap each route. */
-// function asyncHandler(cb){
-//   return async(req, res, next) => {
-//     try {
-//       await cb(req, res, next)
-//     } catch(error){
-//       // Forward error to the global error handler
-//       next(error);
-//     }
-//   }
-// }
+ function asyncHandler(cb){
+   return async(req, res, next) => {
+     try {
+       await cb(req, res, next)
+     } catch(error){
+       // Forward error to the global error handler
+       next(error);
+     }
+   }
+ }
 
 /* GET books listing. */
-router.get('/',  async (req, res) => {
+router.get('/',  asyncHandler(async (req, res) => {
   const books = await Book.findAll();
   res.render('books/index', {books});
-});
+}));
+
+
+router.get('/new_book.html', asyncHandler(async (req, res) => {
+    res.render('books/new-book', { book: {}});
+}));
+
 
 module.exports = router;
